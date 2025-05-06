56413dd8bbcf4d8b3a91e9dac36a0664e0dc1a22
sydNW <aswansyd51@gmail.com>
2025-05-06 22:47:17 +0300

    Update datecalculator.py

--- datecalculator.py
+++ datecalculator.py
@@ -3,7 +3,6 @@ class DateCalculator:
         self.daysOfTheWeek = ["Saturday", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
         self.day = day
 
-        # Adjust month and year if January or February
         if month < 3:
             self.month = month + 12
             self.year = year - 1
@@ -17,6 +16,6 @@ class DateCalculator:
         day_index = (self.day + (13 * (self.month + 1)) // 5 + k + (k // 4) + (j // 4) + 5 * j) % 7
         return self.daysOfTheWeek[day_index]
 
-# Example usage
+# Example 
 datecalculator1 = DateCalculator(8, 7, 2006)
 print(datecalculator1.getDay())  # Output: Saturday
